2a1730e8814e28eba0995197d3cd698598dee081
Alex Beaver <alex@abvr.xyz>
2020-02-24 17:36:07 -0800

    More Javadoc

--- src/main/java/frc/robot/FRCLib/Cyclone/ChaCha/ChaChaPathLibrary.java
+++ src/main/java/frc/robot/FRCLib/Cyclone/ChaCha/ChaChaPathLibrary.java
@@ -1,4 +1,7 @@
 package frc.robot.FRCLib.Cyclone.ChaCha;
 
+/**
+ * This file contains the paths that are commonly used in a ChaChaController
+ */
 public class ChaChaPathLibrary {
 }
--- src/main/java/frc/robot/FRCLib/Cyclone/ChaCha/ChaChaRelativePoint.java
+++ src/main/java/frc/robot/FRCLib/Cyclone/ChaCha/ChaChaRelativePoint.java
@@ -8,7 +8,17 @@ import frc.robot.Subsystems;
  * This is designed for use with the ChaCha slide
  */
 public class ChaChaRelativePoint extends ChaChaPoint {
+    /**
+     * A reference to all subsystems on the robot
+     */
     public Subsystems subsystems;
+
+    /**
+     * Create a relative trajectory point
+     * @param x the x offset in meters
+     * @param y the y offset in meters
+     * @param subsystems the robot subsystems
+     */
     public ChaChaRelativePoint(double x, double y, Subsystems subsystems) {
         super(subsystems.drivetrain.getPose().getTranslation().getX()+x, subsystems.drivetrain.getPose().getTranslation().getY() + y, 0, true);
         this.subsystems = subsystems;
