571acbf9343df0c1789118d3382f5d522e08053e
Alex Beaver <alex@abvr.xyz>
2020-02-26 19:48:30 -0800

    Update ChaChaCriticalPoints.java

--- src/main/java/frc/robot/FRCLib/Cyclone/ChaCha/ChaChaCriticalPoints.java
+++ src/main/java/frc/robot/FRCLib/Cyclone/ChaCha/ChaChaCriticalPoints.java
@@ -1,9 +1,19 @@
 package frc.robot.FRCLib.Cyclone.ChaCha;
 
 public class ChaChaCriticalPoints {
+    //TODO see if y needs to be inverted
     public static ChaChaPoint frontOfLoadingTriangle = new ChaChaPoint(-6.08, 1.85);
     public static ChaChaPoint loadingZoneAtLine = new ChaChaPoint(-3.75, 1.85);
     public static ChaChaPoint lineUpForControlPanel = new ChaChaPoint(-1.14, 0);
     public static ChaChaPoint loadingSideNoReturn = new ChaChaPoint(-2.34, 0.85);
     public static ChaChaPoint insideStructureTurnPoint = new ChaChaPoint(0.43, 2.06);
+    public static ChaChaPoint onTopOfFirstBall = new ChaChaPoint(1.14, 0);
+    public static ChaChaPoint onTopOfSecondBall = new ChaChaPoint(2.07, 0);
+    public static ChaChaPoint onTopOfThirdBall = new ChaChaPoint(2.97, 0);
+    public static ChaChaPoint shootLineDirectLine = new ChaChaPoint(6.03, 1.69);
+    public static ChaChaPoint colorWheelCenterpoint = new ChaChaPoint(0, 0);
+    public static ChaChaPoint twoBallsNearControlPanel = new ChaChaPoint(2.98, 1.99);
+    public static ChaChaPoint threeAwayBallsInsideStruct = new ChaChaPoint(2.66, 3.06);
+    public static ChaChaPoint threeAwayBallsOutsideStruct = new ChaChaPoint(3.92,3.61);
+    public static ChaChaPoint shootTriangle = new ChaChaPoint(8.37, 1.69);
 }
