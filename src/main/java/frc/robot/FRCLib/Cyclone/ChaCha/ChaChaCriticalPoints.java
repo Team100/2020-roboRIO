bcf1d18ae237924a1729d8a6b1991baee4af0ed3
Alex Beaver <alex@abvr.xyz>
2020-02-26 19:44:56 -0800

    Begin adding ChaChaCriticalPoints

---
+++ src/main/java/frc/robot/FRCLib/Cyclone/ChaCha/ChaChaCriticalPoints.java
@@ -0,0 +1,9 @@
+package frc.robot.FRCLib.Cyclone.ChaCha;
+
+public class ChaChaCriticalPoints {
+    public static ChaChaPoint frontOfLoadingTriangle = new ChaChaPoint(-6.08, 1.85);
+    public static ChaChaPoint loadingZoneAtLine = new ChaChaPoint(-3.75, 1.85);
+    public static ChaChaPoint lineUpForControlPanel = new ChaChaPoint(-1.14, 0);
+    public static ChaChaPoint loadingSideNoReturn = new ChaChaPoint(-2.34, 0.85);
+    public static ChaChaPoint insideStructureTurnPoint = new ChaChaPoint(0.43, 2.06);
+}
