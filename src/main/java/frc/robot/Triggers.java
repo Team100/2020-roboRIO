1e771fa30fd2cc31dbdcb95ef87a52b126a3c1e0
Alex Beaver <alexcbeaver@outlook.com>
2020-02-28 20:27:06 -0800

    Add JavaDoc

--- src/main/java/frc/robot/Triggers.java
+++ src/main/java/frc/robot/Triggers.java
@@ -9,17 +9,43 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Subsystems;
+import frc.robot.commands.stateTransitions.TriggerMap;
 
 /**
- * Add your docs here.
+ * All of the virtual triggers for state transitions on the robot
  */
 public class Triggers {
+
+    /**
+     * The subsystems that the Trigger has access to
+     */
+    public Subsystems subsystems;
+
+    /**
+     * The triggermap for all of the commands
+     */
+    public TriggerMap triggerMap;
+
+    /**
+     * The trigger for everything that happens re: the indexerEntranceSensor
+     */
     public Trigger indexerEntranceSensor;
+
+    /**
+     * The trigger for everthing that happens re: the indexerExitSensor
+     */
     public Trigger indexerExitSensor;
 
+    /**
+     * Create a new instance of all of the triggers
+     * @param subsystems the subsystems that can be impacted
+     */
     public Triggers(Subsystems subsystems) {
+        this.subsystems = subsystems;
         indexerEntranceSensor = new Trigger(subsystems.stageOne::getSensorValue);
         indexerExitSensor = new Trigger(subsystems.stageTwo::getSensorValue);
+
+        this.triggerMap = new TriggerMap(this.subsystems);
     }
 
    
