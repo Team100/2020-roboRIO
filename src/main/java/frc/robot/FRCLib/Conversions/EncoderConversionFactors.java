3dd6dca959265229f552c22d1f4d3de20ae34550
Alex Beaver <alex@abvr.xyz>
2020-03-01 13:08:22 -0800

    EncConvFac

--- src/main/java/frc/robot/FRCLib/Conversions/EncoderConversionFactors.java
+++ src/main/java/frc/robot/FRCLib/Conversions/EncoderConversionFactors.java
@@ -7,6 +7,8 @@
 
 package frc.robot.FRCLib.Conversions;
 
+import frc.robot.Constants;
+
 /**
  * Add your docs here.
  */
@@ -31,6 +33,8 @@ public class EncoderConversionFactors {
         } else {
             encoderTicksMA3 = (int) ((angle / 360) * 4096);
         }
+
+        encoderTicksMA3 += Constants.IntakeConstants.IntakeMotionParameters.INTAKE_ZERO;
         return encoderTicksMA3;
     }
 
