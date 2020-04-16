d6ea83d5a4f5954da2a12acc3b790216772766ab
Alex Beaver <alexcbeaver@outlook.com>
2020-04-16 12:22:32 -0700

    Update NebulaRectangularObstruction.java

--- src/main/java/frc/robot/FRCLib/Cyclone/twister/nebula/map/NebulaRectangularObstruction.java
+++ src/main/java/frc/robot/FRCLib/Cyclone/twister/nebula/map/NebulaRectangularObstruction.java
@@ -10,19 +10,20 @@ package frc.robot.FRCLib.Cyclone.twister.nebula.map;
 import frc.robot.FRCLib.Cyclone.twister.nebula.NebulaCenterpointOffset;
 
 /**
- * Add your docs here.
+ * A rectangular obstruction for the NebulaMap. A robot cannot pass through a NebulaRectangularObstruction
  */
 public class NebulaRectangularObstruction implements NebulaObstruction {
 
     public NebulaMapUnits units;
     public NebulaCenterpointOffset offset;
+    public double delta;
     public double left;
     public double right;
     public double bottom;
     public double top;
 
     /**
-     * Create a regular obstruction using field centric meters
+     * Create a rectangular obstruction using field centric meters
      * @param left
      * @param right
      * @param bottom
@@ -36,18 +37,70 @@ public class NebulaRectangularObstruction implements NebulaObstruction {
         this.top = top;
     }
 
-    public NebulaRectangularObstruction(double left, double right, double bottom, double top, NebulaMapUnits units, NebulaCenterpointOffset offset){
-        if(units != NebulaMapUnits.CENTERPOINT_METERS && units != NebulaMapUnits.CENTERPOINT_FEET){
+    /**
+     * Create a rectangular obstruction using field centric units
+     * @param left
+     * @param right
+     * @param bottom
+     * @param top
+     * @param units
+     */
+    public NebulaRectangularObstruction(double left, double right, double bottom, double top, NebulaMapUnits units){
+        if(units != NebulaMapUnits.FIELD_CENTRIC_METERS && units != NebulaMapUnits.FIELD_CENTRIC_FEET){
         
-            throw new IllegalArgumentException("Improper units when creating a NebulaRectangularObstruction. When creating a NebulaRectangularObstruction with an offset, the units must be CENTERPOINT_METERS or CENTERPOINT_FEET. When creating the rectange at "+this.left+","+this.top+", you are sending "+units.toString());
+            throw new IllegalArgumentException("Improper units when creating a NebulaRectangularObstruction. When creating a NebulaRectangularObstruction with only dimensions and units, the units must be FIELD_CENTRIC_METERS or FIELD_CENTRIC_FEET. When creating the rectangle at "+this.left+","+this.top+", you are sending "+units.toString());
         }
-        this.offset = offset;
-        this.units = NebulaMapUnits.FIELD_CENTRIC_METERS;
+        this.units = units;
         this.left = left;
         this.right = right;
         this.bottom = bottom;
         this.top = top;
     }
+
+    /**
+     * Create a rectangular obstruction using points relative to the centerpoint.
+     * @param left
+     * @param right
+     * @param bottom
+     * @param top
+     * @param units
+     * @param offset
+     */
+    public NebulaRectangularObstruction(double left, double right, double bottom, double top, NebulaMapUnits units, NebulaCenterpointOffset offset){
+        if(units != NebulaMapUnits.CENTERPOINT_METERS && units != NebulaMapUnits.CENTERPOINT_FEET){
+        
+            throw new IllegalArgumentException("Improper units when creating a NebulaRectangularObstruction. When creating a NebulaRectangularObstruction with an offset, the units must be CENTERPOINT_METERS or CENTERPOINT_FEET. When creating the rectangle at "+this.left+","+this.top+", you are sending "+units.toString());
+        }
+        this.offset = offset;
+        this.units = units;
+        this.left = left;
+        this.right = right;
+        this.bottom = bottom;
+        this.top = top;
+    }
+
+    /**
+     * Create a rectangular obstruction using grid deltas.
+     * @param left
+     * @param right
+     * @param bottom
+     * @param top
+     * @param units
+     * @param delta
+     */
+    public NebulaRectangularObstruction(double left, double right, double bottom, double top, NebulaMapUnits units, double delta){
+        if(units != NebulaMapUnits.GRID_CENTRIC){
+        
+            throw new IllegalArgumentException("Improper units when creating a NebulaRectangularObstruction. When creating a NebulaRectangularObstruction with a delta, the units must be GRID_CENTRIC. When creating the rectangle at "+this.left+","+this.top+", you are sending "+units.toString());
+        }
+        this.delta = delta;
+        this.units = units;
+        this.left = left;
+        this.right = right;
+        this.bottom = bottom;
+        this.top = top;
+    }
+
 
     /**
      * Get the left bounds in field-centric meters
